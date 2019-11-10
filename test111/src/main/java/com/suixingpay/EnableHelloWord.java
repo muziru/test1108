97ffbd0c5b08662581cd5d63e2b82f97c96fba4a
li_ru <li_ru@suixingpay.com>
2019-11-10 20:20:50 +0800

    Initial commit

---
+++ test111/src/main/java/com/suixingpay/EnableHelloWord.java
@@ -0,0 +1,19 @@
+package com.suixingpay;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description:
+ * @author:muziru
+ * @date:2019/11/07
+ * @version:v1.0
+ */
+@Configuration
+public class EnableHelloWord {
+    public EnableHelloWord(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ){
+        System.out.print("sldls");
+    }
+}
